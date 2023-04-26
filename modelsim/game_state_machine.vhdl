3d33806b4f0dc8a1f88a0d29ce258f7c5192e518
Oliver Wright <owri753@UoA.auckland.ac.nz>
2023-04-26 17:01:07 +1200

    update: Added initial ModelSim project

---
+++ modelsim/game_state_machine.vhdl
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity game_state_machine is port(Clk: in std_logic);
+end entity game_state_machine;
+
+architecture behave of game_state_machine is
+
+begin
+
+end architecture;
