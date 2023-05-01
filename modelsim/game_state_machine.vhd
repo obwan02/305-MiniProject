fc6e5f1249311f2fb51d5026eb6e66493e1bd047
Janith Hettiarachchi <100389922+jhet879@users.noreply.github.com>
2023-05-01 16:45:02 +1200

    update(game_state_machine.vhd): Implemented FIND_NEXT_STATE process

--- modelsim/game_state_machine.vhd
+++ modelsim/game_state_machine.vhd
@@ -8,23 +8,27 @@ entity game_moore_fsm is port(
 	Dead: in std_logic;
 	Start: in std_logic;
 	Train : in std_logic;
+	TryAgain: in std_logic;
 	GameRunning: out std_logic;
 	TrainingStatus: out std_logic);
 end entity game_moore_fsm;
 
 architecture behaviour of game_moore_fsm is
-	type fsm_states is (Initial, TrainingSelection, TrainingMode, NormalMode, GameOverMode);
+	type fsm_states is (Initial, 
+						TrainingSelection, 
+						TrainingMode, 
+						NormalMode, 
+						GameOverMode);
 	signal current_state, next_state : fsm_states;					 
 begin
 
 -- STATE_CHANGER Process
 --
--- This process is triggered by the clock and
--- performs the transitioning of the states.
+-- This process is triggered by the clock and performs the transitioning
+-- of the states.
 --
--- If the clock in on the rising edge, check
--- if reset is set, else, set the current
--- state to the next state.
+-- If the clock in on the rising edge, check if reset is set, else, set
+-- the current state to the next state.
 STATE_CHANGER : process (Clk)
 begin
 	if rising_edge(Clk) then
@@ -35,4 +39,43 @@ begin
 		end if;
 	end if;
 end process;
+
+-- FIND_NEXT_STATE Process
+--
+-- This process is triggered by the the current_state, and all of the
+-- input signals.
+-- 
+-- Given the current state of the FSM, if the relevant conditions are
+-- met, set the next_state to the corresponding next state.
+FIND_NEXT_STATE : process (current_state, Reset, Dead, Start, Train)
+begin
+	next_state <= Initial;
+	case (current_state) is
+		when Initial =>
+			if (Start = '1') then
+				next_state <= TrainingSelection;
+			end if;
+		when TrainingSelection  =>
+			if (Train = '1') then
+				next_state <= TrainingMode;
+			else
+				next_state <= NormalMode;
+			end if;
+		when TrainingMode =>
+			if (Dead = '1') then
+				next_state <= GameOverMode;
+			end if;
+		when NormalMode =>
+			if (Dead = '1') then
+				next_state <= GameOverMode;
+			end if;
+		when GameOverMode =>
+			if (TryAgain = '1') then
+				next_state <= Initial;
+			end if;
+		when others =>
+			next_state <= Initial;
+	end case;
+end process;
+
 end architecture;
