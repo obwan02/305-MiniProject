2555459966d114dfa581297d16b8e86f9497654b
obwan02 <obwan02@hotmail.co.nz>
2023-05-07 22:49:18 +1200

    update: Improving movement

--- modelsim/player_update.vhd
+++ modelsim/player_update.vhd
@@ -17,30 +17,49 @@ end entity player_update;
 architecture rtl of player_update is
 begin
     process(Clk)
-        constant MaxOut: signed(9 downto 0) := to_signed(479, 10);  
+        constant MAX_OUT: signed(9 downto 0) := to_signed(479, 10);  
     
-        variable YVelocity: signed(9 downto 0) := to_signed(1, 10);  
-        variable CurrentY : signed(9 downto 0);
+		-- Here, we keep track of the current
+		-- y velocity, and position
+        variable v_YVel: signed(9 downto 0) := to_signed(1, 10);  
+        variable v_CurrentY: signed(9 downto 0);
+
+		-- This is used to keep
+		-- track of the previous state
+		-- of the left mouse button
+		variable v_PrevLeftMB: std_logic := '0';
     begin
 
-    if rising_edge(Clk) then
-        if (LeftMouseButton /= '0') then 
-            if (CurrentY - YVelocity <= 0) then
-                YVelocity := (others => '0');
-            else
-                YVelocity := YVelocity + 1;
-            end if;
-        else
-            if (CurrentY + YVelocity >= 479) then
-                YVelocity := (others => '0');
-            else
-                YVelocity := YVelocity - 10;
-            end if;
-        end if;
+		if rising_edge(Clk) then
 
-        CurrentY := CurrentY - YVelocity;
-        NewY <= to_signed(100, 10);
-    end if;
+			-- Change our velocity so that we
+			-- go upwards if the LMB was clicked.
+			-- Otherwise, we increment the velocity to
+			-- simulate gravity.
+			if (LeftMouseButton /= v_PrevLeftMB) then 
+				v_YVel := -10;
+			else
+				v_YVel := v_YVel + 1;
+			end if;
+
+			-- Here, we put a cap on the amount of downwards
+			-- velocity we can obtain
+			if v_YVel <= 10 then
+				v_Yvel := 10; 
+			end if;
+
+			v_CurrentY := v_CurrentY + v_YVel;
+			
+			-- Once we have updated the velocity,
+			-- we check if we are out of bounds
+			-- TODO: wait for collision checking results
+
+
+			NewY <= v_CurrentY;
+
+			v_PrevLeftMB <= LeftMouseButton;
+
+		end if;
     
     end process;
 
